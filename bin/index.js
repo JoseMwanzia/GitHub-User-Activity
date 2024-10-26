37c1a8abda8ebe5ee5c36fb093c3c4b3bd25df78
joseph mwanzia <makamwanzia@gmail.com>
2024-10-26 23:32:30 +0300

    display all the users events

--- bin/index.js
+++ bin/index.js
@@ -15,3 +15,26 @@ const fetchedUser =  async (username) => {
 
   return response.json()
 }
+async function displayEvents(events, filtereEvent) {
+  if (events.length === 0) {
+    console.log('No recent activity!')
+    return;
+  }
+  events.forEach((event) => {
+    let commitCount;
+
+    if (event.type === 'PushEvent') {
+      commitCount = `Pushed ${event.payload.commits.length} to ${event.repo.name}`
+    } else if (event.type === 'IssueEvent') {
+      commitCount = `Issues events were ${event.payload.commits.length}`
+    } else if (event.type === 'ForkEvent') {
+      commitCount = `Fork events are ${event.payload.commits.length}`
+    } else if (event.type === 'CreateEvent') {
+      commitCount = `Create events are ${event.payload.ref_type}`
+    } else {
+      commitCount = `Not yet created event`;
+    }
+    console.log(commitCount)
+  })
+}
+
