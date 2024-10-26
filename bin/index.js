ab9c6718b96cf5203bd193dc63bfba165d0a285d
joseph mwanzia <makamwanzia@gmail.com>
2024-10-26 23:26:10 +0300

    fetched data

---
+++ bin/index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const fetchedUser =  async (username) => {
+  const response  = await fetch(`https://api.github.com/users/${username}/events`, {
+    headers: {'Accept': 'application/vnd.github+json'}
+  })
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      throw new Error("User not found. Please check the username.")
+    } 
+    else {
+      throw new Error(`Error fetching data: ${response.status}`)
+    }
+  }
+
+  return response.json()
+}
